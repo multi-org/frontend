347b082864e4579737f1210d2c6ec0f8d2ef8aa4
Lucas Henrique <henrique.lucas521@gmail.com>
2024-08-05 22:15:36 -0300

    add icons

---
+++ src/components/custom-icons/bank-icon.tsx
@@ -0,0 +1,15 @@
+import { IconProps } from "./types";
+
+export function BankIcon({ size }: IconProps) {
+  return (
+    <svg width={size} height={size} viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect width="56" height="56" rx="28" fill="#54A3AC"/>
+      <path d="M16 39.3333H40" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M20 34V24.6667" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M25.3333 34V24.6667" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M30.6667 34V24.6667" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M36 34V24.6667" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M28 12.6667L38.6666 19.3333H17.3333L28 12.6667Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>   
+  )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,28 @@
 import { Header } from "@/components/custom/Header";
-import bgImage from '../../assets/home-bg-image.jpg'
+
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+import { BankIcon } from "@/components/custom-icons";
+
 
 export function Home() {
-  console.log(bgImage)
   return (
-		<>
-  	  <Header />
-			<div className="flex flex-col w-full items-center">
+    <>
+      <Header />
+      <div className="flex flex-col w-full items-center">
 				<div id="home" className={`flex bg-center w-full justify-center items-center bg-[url('/src/assets/home-bg-image.jpg')] h-[600px] bg-no-repeat from-transparent to-white`}>
           <div className="flex flex-col items-center text-grayLight justify-center">
             <h1 className="text-6xl font-bold">MULTI</h1>
             <h1 className="text-3xl font-bold text-center">UEPB</h1>
             <p className="w-40 text-center mt-6">Um breve slogan do projeto</p>
           </div>
-				</div>
+        </div>
         <div className="w-full h-36 mt-[-144px] bg-gradient-to-b from-transparent to-white">
         </div>
         <div id="about-us" className="w-full flex flex-col items-center pt-20 text-center">
           <h1 className="font-bold text-2xl flex flex-col items-center">
-            Quem somos? 
-            <hr className="w-24 border-[1.5px] border-orangeNormal mt-[-4px]"/>
+            Quem somos?
+            <hr className="w-24 border-[1.5px] border-orangeNormal mt-[-4px]" />
           </h1>
           <div className="flex justify-between w-[670px] items-center mt-24">
             {/* <img src="" alt="" /> */}
@@ -27,16 +30,29 @@ export function Home() {
             <p className="w-72">
               A plataforma Multi UEPB é projetada para ser uma solução abrangente e eficiente para o gerenciamento de aluguel de espaços, serviços e equipamentos em diversas instituições, atendendo a diferentes tipos de usuários: individuais, empresas, instituições públicas e instituições públicas/privadas.
             </p>
-          </div> 
+          </div>
         </div>
         <div id="features" className="">
-          AAAAAAAAAAAAAAAAA
+          <h1 className="font-bold text-2xl flex flex-col items-center">
+            Quem somos?
+            <hr className="w-24 border-[1.5px] border-orangeNormal mt-[-4px]" />
+          </h1>
+          <div className="pt-20">
+            <Card>
+              <CardHeader>
+                <BankIcon size={50} />
+              </CardHeader>
+              <CardContent>
+
+              </CardContent>
+            </Card>
+          </div>
         </div>
         <div id="faq" className="">
           AAAAAAAAAAAAAAAAAAAAAAA
         </div>
-			</div>
-		</>
-		
-	)
+      </div>
+    </>
+
+  )
 }
