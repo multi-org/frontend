9f97af385c63583968985639a9972d8a6e1decc1
HenriqueFMA <carolinahenriquef@gmail.com>
2025-06-27 01:26:12 -0300

    style(ConfirmationOfValidation): add background color and text color for improved readability

--- src/components/custom/Register/stpes/EmailValidation/ConfirmationOfValidation.tsx
+++ src/components/custom/Register/stpes/EmailValidation/ConfirmationOfValidation.tsx
@@ -3,7 +3,7 @@ import { CheckCircle2 } from "lucide-react";
 
 export default function ConfirmationOfValidation() {
     return (
-        <Card className="w-full max-w-md border-white">
+        <Card className="w-full max-w-md border-white bg-[#F2F2F2]">
             <CardHeader className="flex flex-col items-center">
             <CheckCircle2 className="text-green-500 w-16 h-16 mb-2 animate-rote-in" />
                 <CardTitle className="text-center">
@@ -11,7 +11,7 @@ export default function ConfirmationOfValidation() {
                 </CardTitle>
             </CardHeader>
             <CardContent className="flex flex-col items-center gap-4">
-                <p className="text-muted-foreground text-center">
+                <p className="text-muted-foreground text-center text-gray-700">
                     Seu endereço de e-mail foi confirmado. Agora você pode acessar todos os recursos da plataforma.
                 </p>
                 
