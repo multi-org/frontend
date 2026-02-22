ef6d87d6046dda687be87085e74b76694a1fce58
aldoalbuquerquejr <aldocandeia000@gmail.com>
2026-02-22 14:29:39 -0300

    updated api

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://77.37.43.4:8083/",
-  baseURL: "http://localhost:8083",
+  baseURL: "http://77.37.43.4:8083/",
+  // baseURL: "http://localhost:8083",
   withCredentials: true, 
 
 });
